82a24849bdf2d86f351b27d9becc058bfe9470bc
ying <82625637+yzhang-mnv@users.noreply.github.com>
2023-05-04 15:23:33 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
-streamlit.text('Fruityvice Fruit Advice!')
+streamlit.header('Fruityvice Fruit Advice!')
 
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
