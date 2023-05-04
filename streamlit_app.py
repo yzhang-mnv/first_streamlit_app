2d57dedd7a31c08b8cb1daee974b5044fa298a86
ying <82625637+yzhang-mnv@users.noreply.github.com>
2023-05-04 16:01:05 +1000

    Update streamlit_app.py
    fix bug on 27.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-#import requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 #streamlit.text(fruityvice_response.json())
 
